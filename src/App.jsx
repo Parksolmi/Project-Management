2bd7c5a6214832704fbbee26f578549cdf4282bb
Solmi <7548ck@naver.com>
2024-03-10 18:43:12 +0900

    Feat: 프로젝트 삭제 기능

--- src/App.jsx
+++ src/App.jsx
@@ -23,21 +23,6 @@ function App() {
     });
   }
 
-  function handleAddProject (projectData) {
-    setProjectsState(prevState => {
-      const newProject = {
-        ...projectData,
-        id: Math.random(),
-      };
-
-      return {
-        ...prevState,
-        selectedProjectId: undefined,
-        projects: [...prevState.projects, newProject],
-      }
-    })
-  }
-
   function handleCancelAddProject () {
     setProjectsState(prevState => {
       return {
@@ -56,8 +41,35 @@ function App() {
     })
   }
 
+  function handleAddProject (projectData) {
+    setProjectsState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      }
+    })
+  }
+
+  function handleDeleteProject () {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      }
+    })
+  }
+
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
-  let content = <SelectedProject project={selectedProject}/>;
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
 
   if(projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
